cdeeb16a0f399249a19d60ca4eb284d5d5f8756f
alecanthony <alecanthony39@gmail.com>
2023-05-17 19:55:55 -0500

    part 2

--- db/index.js
+++ db/index.js
@@ -332,6 +332,25 @@ async function getPostsByTagName(tagName) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `,
+      [username]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function getAllTags() {
   const { rows } = await client.query(`SELECT * FROM tags;`);
 
@@ -354,4 +373,5 @@ module.exports = {
   addTagsToPost,
   getPostsByTagName,
   getAllTags,
+  getUserByUsername,
 };
